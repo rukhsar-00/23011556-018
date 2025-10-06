074d164e4d760d8567309ce35d68d55e99d05430
hp <hp@DESKTOP-1117BUV>
2025-10-06 21:11:21 +0500

    Program 4

---
+++ Grade eva.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23011556_018
+{
+    internal class Grade_eva
+    {
+    }
+}
