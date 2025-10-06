2c0e7bef4d912cdfa87b5ab4094172b22a27684a
hp <hp@DESKTOP-1117BUV>
2025-10-06 21:22:21 +0500

    Program 10

---
+++ Array Count Even and Odd num.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23011556_018
+{
+    internal class Array_Count_Even_and_Odd_num
+    {
+    }
+}
