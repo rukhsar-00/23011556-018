6275a6d61d9e94da73a7dc13520140414ca038f3
hp <hp@DESKTOP-1117BUV>
2025-10-06 20:44:32 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23011556_018
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
